74f2db22a96e73fe93e0db6632e048b59075a40c
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-30 19:22:51 +0100

    cornify.js added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const pressed = [];
+const secretCode = "sebastian";
+
+
 window.addEventListener("keyup", (e) => {
     console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    console.log(pressed);
 })
