d6faf8405bd43621a4a3fea70a771257acfb8cd6
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-30 19:27:56 +0100

    added styles

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 const pressed = [];
-const secretCode = "sebastian";
+const secretCode = "i will hire you";
 
 
 window.addEventListener("keyup", (e) => {
     console.log(e.key);
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join("").includes(secretCode)) {
+        cornify_add();
+    }
     console.log(pressed);
 })
